f28b43619921fbcafaac915b8db6214252df0f84
Henry Betancourth <noelys215@gmail.com>
2026-07-14 13:07:57 -0400

    chore: add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import AppProviders from "./app/providers";
+import { Analytics } from "@vercel/analytics/next";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AppProviders>
+      <Analytics />
       <App />
     </AppProviders>
   </StrictMode>,
